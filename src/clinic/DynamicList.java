7a6c4a6bf58d9087bccf614174f9bf230ccf95e7
Adriekl <adrielvillegaslopez@gmail.com>
2026-08-13 14:05:28 -0600

    Creacion de lista dinamica
    Se crea la lista la lista dinamica y sus tipo y key en modo parametro dinamico

---
+++ src/clinic/DynamicList.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package clinic;
+
+/**
+ *
+ * @author Adriel
+ * @param <T>
+ * @param <K>
+ */
+public interface DynamicList<T,K> {
+    public boolean add(T item);
+    public int size();
+    public boolean isEmpty();
+    
+}
