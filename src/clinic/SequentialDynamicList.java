e05a878d51936d4c2e525abf20ba656077124644
Adriekl <adrielvillegaslopez@gmail.com>
2026-08-13 14:15:14 -0600

    creacion de sequeantial list
    se crea con un metodo nuevo y extiende de dynamic list

---
+++ src/clinic/SequentialDynamicList.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package clinic;
+
+/**
+ *
+ * @author Adriel\
+ * @param <T>
+ * @param <K>
+ */
+public interface SequentialDynamicList<T,K> extends DynamicList<T,K>{
+    public T get();
+    public boolean remove();
+    
+    
+    
+}
